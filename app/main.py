13a963d951c27ea6f551313883e469f2cb0808d6
Raj Dhumal <rajd810@outlook.com>
2023-07-11 21:19:11 +0530

    Added few feature for Experience and skills page

--- app/main.py
+++ app/main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/experience')
+def experience():
+    return render_template("experience.html")
+
+@app.route('/skills')
+def skills():
+    return render_template("skills.html")
+
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5001, debug = True)
