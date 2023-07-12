3cab2d83a4d5246a056fada756c6cb9e80366978
Raj Dhumal <rajd810@outlook.com>
2023-07-12 21:47:03 +0530

    Changes did in flask webpages

--- app/main.py
+++ app/main.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/about')
+def index():
+    return render_template('about.html')
 
 @app.route('/experience')
 def experience():
